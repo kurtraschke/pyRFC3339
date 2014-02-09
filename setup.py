5152d92ee9475533d4d79b8555ed3d9789376957
Kurt Raschke <kurt@kurtraschke.com>
2014-02-09 16:32:51 -0500

    Bump version for new release with Python 3 compatibility.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pyRFC3339",
-    version = "0.1",
+    version = "0.2",
 
     author = "Kurt Raschke",
     author_email = "kurt@kurtraschke.com",
