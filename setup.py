9d57ef60037df9fb81555a8d000246b23505aced
Kurt Raschke <kurt@kurtraschke.com>
2015-11-08 22:19:49 -0500

    Updates to packaging.

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.rst", "r") as readme:
+    long_description = readme.read()
 
 setup(
     name = "pyRFC3339",
     version = "0.2",
-
     author = "Kurt Raschke",
     author_email = "kurt@kurtraschke.com",
+    url = "https://github.com/kurtraschke/pyRFC3339",
     description = "Generate and parse RFC 3339 timestamps",
+    long_description = open("README.rst").read(),
     keywords = "rfc 3339 timestamp",
     license = "MIT",
-
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -18,9 +21,9 @@ setup(
         "Topic :: Internet"
         ],
 
-    packages = find_packages(),
+    packages = ['pyrfc3339'],
 
     install_requires = ['pytz'],
     test_suite = 'nose.collector',
     tests_require = ['nose']
-    )
+)
