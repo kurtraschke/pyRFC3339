5a185adab33f326b455fc39731c1d3b1d7b97855
Kurt Raschke <kurt@kurtraschke.com>
2015-11-08 22:47:31 -0500

    Use datetime.timedelta.total_seconds() when available.

--- pyrfc3339/utils.py
+++ pyrfc3339/utils.py
@@ -77,8 +77,13 @@ class FixedOffset(tzinfo):
 
 def timedelta_seconds(td):
     '''
-    Return the offset stored by a timedelta object as an integer number of
-    seconds.  Microseconds, if present, are rounded to the nearest second.
+    Return the offset stored by a :class:`datetime.timedelta` object as an
+    integer number of seconds.  Microseconds, if present, are rounded to
+    the nearest second.
+
+    Delegates to
+    :meth:`timedelta.total_seconds() <datetime.timedelta.total_seconds()>`
+    if available.
 
     >>> timedelta_seconds(timedelta(hours=1))
     3600
@@ -95,11 +100,14 @@ def timedelta_seconds(td):
 
     '''
 
-    days = td.days
-    seconds = td.seconds
-    microseconds = td.microseconds
+    try:
+        return int(round(td.total_seconds()))
+    except AttributeError:
+        days = td.days
+        seconds = td.seconds
+        microseconds = td.microseconds
 
-    return int(round((days * 86400) + seconds + (microseconds / 1000000)))
+        return int(round((days * 86400) + seconds + (microseconds / 1000000)))
 
 
 def timezone(utcoffset):
